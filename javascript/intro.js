9eaa5b6ad418c668162a5f4588afce05850aa097
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-03 11:29:19 +0300

    variable

---
+++ javascript/intro.js
@@ -0,0 +1,25 @@
+// number
+2 + 3
+//string 
+"yahya"
+//alert
+alert("yahya")
+// variable
+var myName = "yahya";
+var yourName = prompt("What is your name?");
+alert("my name is "+myName + ",Welcome to my course" + yourName + "!");
+
+var gamelevel = 1;
+gamelevel = 2;
+gamelevel = 3;
+alert("your gamelevel is:"+ gamelevel); //it will alert your gamelevel is 3
+
+//exercise swap two number
+var a = "3";
+var b = "8";
+
+var temp = a;
+a = b;
+b = temp;
+console.log('a is '+ a); // a = 8
+console.log('a is '+ b); // b = 3
