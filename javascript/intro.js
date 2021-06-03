4aa961e5b4111a64e73a4518db170497bfb6077d
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-03 16:24:12 +0300

    case change

--- javascript/intro.js
+++ javascript/intro.js
@@ -23,3 +23,44 @@ a = b;
 b = temp;
 console.log('a is '+ a); // a = 8
 console.log('a is '+ b); // b = 3
+
+// name your variables understanble name 
+// dont name ur variable like var number character dont space the name (my_name,  my name is not allowed)
+
+//name spacing
+var message = "Hello";
+var name = "yahya";
+alert(message + " there, " + name);
+
+//length
+var name1 = "yahya";
+name1.length;
+
+// calculate the characters written 
+
+var num = "280";
+var num1 = prompt("Enter summary of ur project?");
+var num2 = num1.length;
+alert("you have written "+ num2 + " characters, you have " + (140-num2) + " charcters left");
+
+//slicing slicing(x,y)    bread.slice(x,y)
+
+var nam1 = "yahya";
+nam1.slice(0,1); //y
+alert(prompt("Compose your tweet:").slice(0,140));
+
+
+// .toUppercase
+
+var name = "yahya";
+name.toUpoercase;
+
+//captilize first letter
+
+var yNam = prompt("What is your name?");
+var firstChar = yNam.slice(0,1);
+var firstUpperCase = firstChar.toUpperCase();// change rest of letters to upper case
+var restOfChar = yNam.slice(1,5);
+restOfChar = restOfChar.toLowerCase(); // change rest of letters to lower case
+var captalizeName = firstUpperCase+restOfChar
+alert(captalizeName);
