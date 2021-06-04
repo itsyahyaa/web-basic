c14234d737c0567c110143ade59ac97fbf8fc0e7
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-04 11:37:46 +0300

    leap yr

--- Advance JS/adv.js
+++ Advance JS/adv.js
@@ -9,5 +9,38 @@ console.log(n); // 1 to 6
 a = prompt("Enter first person's name?"); // first name
 b = prompt("Enter second person's name?"); // last name 
 var c = Math.random()*100; // generate random number
-c = Math.floor(c)+1; // remove decimal
-alert("Your Love Score is "+c+"%");// display number in perstange form
+c = Math.floor(c)+1; // remove decimal0
+if(c>70){
+    alert("Your Love Score is "+c+"% you love each other like kanye loves kanye");// display number in perstange form
+}
+else if(c>30 && c<= 70){
+    alert("Your Love Score is "+c+"%");// display number in perstange form
+}
+else{
+    alert("Your Love Score is "+c+"% you love each other like oil and water");// display number in perstange form
+}
+/*
+=== is equal(datatype is must) ==(doesnot check data type) to  !== is not eqaul to >= greater equal <=less and equal
+*/
+
+
+// leap year challenge
+// on every that is divisivle by 4 except 100 unless the year is divisible 400
+function isLeap(year){
+    if(year % 4 === 0){
+        if(year % 100 === 0){
+            if(year % 400 === 0){
+                alert(year+" is a leap year");
+            }
+            else{
+                alert(year+" is not a leap year");
+            }
+        }else{
+            alert(year+" is a leap year");
+        }
+    }else{
+        alert(year+" is not a leap year");
+    }
+}
+isLeap(2004);
+alert(year+" is a leap year");
