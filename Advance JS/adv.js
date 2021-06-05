db3831cdd56e471f794f9defae30bb255dd8bd3d
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-05 15:13:38 +0300

    date

--- Advance JS/adv.js
+++ Advance JS/adv.js
@@ -144,7 +144,7 @@ function fizzBuzz(){
 fizzBuzz();
 
 //challenge
-// feinonacciGenerator
+// fibonacciGenerator
 var first=0;
 var second=1;
 var sum=0;
@@ -159,3 +159,71 @@ function fibonacci(){
     }
 }
 fibonacci(5);
+//fibonacciGenerator array form
+
+function fibonacci(n){
+    var output = [];
+    if (n===1){
+        output = [0]; // [0]
+    }
+    else if (n===2){
+        output = [0,1]; // [0,1]
+    }
+    else{
+        output = [0,1];
+        for (var i = 2; i < n; i++){
+            output.push(output[output.length - 2] + output[output.length - 1]); // [0,1,1]
+        }
+    
+    }
+    return output;
+}
+
+output = fibonacci(10);
+console.log(output);
+
+// Display current day and time 
+var today = new Date();
+  var day = today.getDay(); // curent day
+  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+  console.log("Today is : " + daylist[day] + "."); // day
+  var hour = today.getHours(); // hour
+  var minute = today.getMinutes();
+  var second = today.getSeconds(); // min
+  var prepand = (hour >= 12)? " PM ":" AM "; //h >  is PM else AM
+  hour = (hour >= 12)? hour - 12: hour;
+  if (hour===0 && prepand===' PM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Noon';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' PM';
+  } 
+  } 
+  if (hour===0 && prepand===' AM ') 
+  { 
+  if (minute===0 && second===0)
+  { 
+  hour=12;
+  prepand=' Midnight';
+  } 
+  else
+  { 
+  hour=12;
+  prepand=' AM';
+  } 
+  } 
+console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+
+//Write a JavaScript program to print date
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+today = dd+'/'+mm+'/'+yyyy;
+console.log(today);
