4c87ff707c322c3754145a7396ff6b36979f596a
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-05 18:09:12 +0300

    manipulation

--- DOM/index.js
+++ DOM/index.js
@@ -8,7 +8,7 @@ document.querySelector("input").click();
 
 //selecting last li
 document.getElementsByTagName("li")[2].style.color = "green";
-document.getElementsByClassName("btn")[0].style.color = "red";
+document.getElementsByClassName("btn")[0].style.color = "black";
 
 document.getElementById("title").style.fontSize="30px";
 // querey selector is easiest you can put id or class
@@ -18,3 +18,9 @@ document.querySelector("h1").innerHTML = "GOod byepoijhgcfx";
 document.querySelector("#list a").style.color = "red";
 
 document.querySelectorAll("#list .list")[2].style.color = "purple"; 
+
+
+// background color red
+document.getElementsByClassName("btn")[0].style.backgroundColor = "red";
+// add class to the html
+document.querySelector(".btn").classList.add("btn1");
