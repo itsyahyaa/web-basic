580d8ae56b146148ced97062f2ec5d0cd8bc42f8
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-05 16:24:40 +0300

    getElementsByClassName

--- DOM/index.js
+++ DOM/index.js
@@ -1,2 +1,11 @@
 // alert("hello");
-document.querySelector("h1").innerHTML = "GOod bye"
+//document.querySelector("h1").innerHTML = "GOod bye"
+var heading = document.firstElementChild.lastElementChild.firstElementChild; // select H1
+heading.innerHTML = "Good bye";
+heading.style.color = "red";
+
+document.querySelector("input").click();
+
+//selecting last li
+document.getElementsByTagName("li")[2].style.color = "green";
+document.getElementsByClassName("btn")[0].style.color = "red";
