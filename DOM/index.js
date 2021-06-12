cc794f645122d535f321af68bb72343377f0525c
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-06-12 22:07:24 +0300

    mr

--- DOM/index.js
+++ DOM/index.js
@@ -26,3 +26,5 @@ document.getElementsByClassName("btn")[0].style.backgroundColor = "red";
 document.querySelector(".btn").classList.add("btn1");
 // chamging attribute
 document.querySelector("a").setAttribute("href", "google.com");
+// chamging attribute
+document.querySelector("a").setAttribute("href", "google.com");
