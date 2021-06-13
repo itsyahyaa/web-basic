82fb30d4b0bd209fa0845520702e6f359fa5a916
Yahya Mohamed <yahyamohamed0001@gmail.com>
2021-06-13 22:35:34 +0300

    ....

--- Advance JS/adv.js
+++ Advance JS/adv.js
@@ -235,3 +235,11 @@ var x = Math.random();
 n = x * 6; // multiply upto the range of number you want 
 n = Math.floor(n)+1;
 console.log(n); // 1 to 6 
+
+
+// generating random number between 0 - 0.9999999999999999
+// arrange for dice
+var x = Math.random();
+n = x * 6; // multiply upto the range of number you want 
+n = Math.floor(n)+1;
+console.log(n); // 1 to 6 
